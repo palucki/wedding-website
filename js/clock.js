a9a0d18f25dcf4467d5aba8fea78dad4a2eb1597
palucki <piotrek.palucki@gmail.com>
2025-06-02 21:44:23 +0200

    Updated time

--- js/clock.js
+++ js/clock.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2025-09-13 14:00", "Europe/Warsaw");
+  let targetDate = moment.tz("2025-09-13 13:00", "Europe/Warsaw"); // Hack for incorrect timezone handling? 
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
